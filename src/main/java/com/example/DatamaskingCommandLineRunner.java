378e1c6eba2c215244685242be6c341c0b3ad481
izuno4t <9846+izuno4t@users.noreply.github.com>
2021-07-31 22:39:23 +0900

    bump up

--- src/main/java/com/example/DatamaskingCommandLineRunner.java
+++ src/main/java/com/example/DatamaskingCommandLineRunner.java
@@ -34,115 +34,108 @@ import com.opencsv.CSVWriter;
 @Component
 public class DatamaskingCommandLineRunner implements CommandLineRunner {
 
-	private final static Logger logger = LoggerFactory.getLogger(DatamaskingCommandLineRunner.class);
+    private final static Logger logger = LoggerFactory.getLogger(DatamaskingCommandLineRunner.class);
 
-	@Option(name = "--spring.output.ansi.enabled", hidden = true, usage = "SpringBoot argument")
-	private String springOutputAnsi = "";
+    @Option(name = "--spring.output.ansi.enabled", hidden = true, usage = "SpringBoot argument")
+    private final String springOutputAnsi = "";
 
-	@Option(name = "-h", aliases = "--help", usage = "print usage message and exit")
-	private boolean usageFlag;
+    @Option(name = "-h", aliases = "--help", usage = "print usage message and exit")
+    private boolean usageFlag;
 
-	@Option(name = "-i", metaVar = "input file name", aliases = "--input", required = true, usage = "入力ファイル")
-	private String input = "";
+    @Option(name = "-i", metaVar = "input file name", aliases = "--input", required = true, usage = "入力ファイル")
+    private final String input = "";
 
-	@Option(name = "-ie", metaVar = "input file encode", aliases = "--input-encode", usage = "入力ファイルの文字コード（未指定の場合はUTF-8）")
-	private String inputEncode = "";
+    @Option(name = "-ie", metaVar = "input file encode", aliases = "--input-encode", usage = "入力ファイルの文字コード（未指定の場合はUTF-8）")
+    private final String inputEncode = "";
 
-	@Option(name = "-o", metaVar = "output file name", aliases = "--output", required = true, usage = "出力ファイル")
-	private String output = "";
+    @Option(name = "-o", metaVar = "output file name", aliases = "--output", required = true, usage = "出力ファイル")
+    private final String output = "";
 
-	@Option(name = "-oe", metaVar = "output file encode", aliases = "--output-encode", usage = "入力ファイルの文字コード（未指定の場合はUTF-8）")
-	private String outputEncode = "";
+    @Option(name = "-oe", metaVar = "output file encode", aliases = "--output-encode", usage = "入力ファイルの文字コード（未指定の場合はUTF-8）")
+    private final String outputEncode = "";
 
-	@Option(name = "-f", metaVar = "fields", aliases = "--fields", usage = "マスク対象列番号")
-	private String fields = "";
+    @Option(name = "-f", metaVar = "fields", aliases = "--fields", usage = "マスク対象列番号")
+    private final String fields = "";
 
-	@Override
-	public void run(String... args) throws Exception {
-		logger.info(ToStringBuilder.reflectionToString(args));
-		logger.info(
-				Arrays.asList(args).stream().collect(Collectors.joining(",", getClass().getSimpleName() + "[", "]")));
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info(ToStringBuilder.reflectionToString(args));
+        logger.info(
+                Arrays.stream(args).collect(Collectors.joining(",", getClass().getSimpleName() + "[", "]")));
 
-		CmdLineParser parser = new CmdLineParser(this);
-		try {
-			parser.parseArgument(args);
-			if (usageFlag) {
-				System.out.println("Usage:");
-				parser.printUsage(System.out);
-				return;
-			}
-		} catch (CmdLineException e) {
-			parser.printUsage(System.err);
-			throw new IllegalArgumentException(e);
-		}
+        CmdLineParser parser = new CmdLineParser(this);
+        try {
+            parser.parseArgument(args);
+            if (usageFlag) {
+                System.out.println("Usage:");
+                parser.printUsage(System.out);
+                return;
+            }
+        } catch (CmdLineException e) {
+            parser.printUsage(System.err);
+            throw new IllegalArgumentException(e);
+        }
 
-		System.out.println("データのマスク化を開始します。");
+        System.out.println("データのマスク化を開始します。");
 
-		Path inputPath = Paths.get("", input);
-		Charset inputCharset = Charset.forName(StringUtils.defaultIfBlank(inputEncode, "UTF-8"));
-		Path outputPath = Paths.get("", output);
-		Charset outputCharset = Charset.forName(StringUtils.defaultIfBlank(outputEncode, "UTF-8"));
-		List<Integer> maskFields = Arrays.stream(StringUtils.split(fields, ",")).map(s -> Integer.valueOf(s))
-				.collect(Collectors.toList());
+        Path inputPath = Paths.get("", input);
+        Charset inputCharset = Charset.forName(StringUtils.defaultIfBlank(inputEncode, "UTF-8"));
+        Path outputPath = Paths.get("", output);
+        Charset outputCharset = Charset.forName(StringUtils.defaultIfBlank(outputEncode, "UTF-8"));
+        List<Integer> maskFields = Arrays.stream(StringUtils.split(fields, ",")).map(Integer::valueOf)
+                .collect(Collectors.toList());
 
-		logger.info("input file=" + inputPath.toAbsolutePath().toString());
-		logger.info("output file=" + outputPath.toAbsolutePath().toString());
+        logger.info("input file={}", inputPath.toAbsolutePath());
+        logger.info("output file={}", outputPath.toAbsolutePath());
 
-		// 読込ファイルはUTF-8に変換しておくこと。
-		try (CSVReader reader = new CSVReader(new BufferedReader(
-				new InputStreamReader(new BufferedInputStream(new FileInputStream(inputPath.toFile())), inputCharset)));
-				CSVWriter writer = new CSVWriter(
-						new BufferedWriter(new OutputStreamWriter(
-								new BufferedOutputStream(new FileOutputStream(outputPath.toFile())), outputCharset)),
-						CSVWriter.DEFAULT_SEPARATOR, CSVWriter.DEFAULT_QUOTE_CHARACTER, CSVWriter.RFC4180_LINE_END)) {
+        // 読込ファイルはUTF-8に変換しておくこと。
+        try (CSVReader reader = new CSVReader(new BufferedReader(
+                new InputStreamReader(new BufferedInputStream(new FileInputStream(inputPath.toFile())), inputCharset)));
+             CSVWriter writer = new CSVWriter(
+                     new BufferedWriter(new OutputStreamWriter(
+                             new BufferedOutputStream(new FileOutputStream(outputPath.toFile())), outputCharset)),
+                     CSVWriter.DEFAULT_SEPARATOR, CSVWriter.DEFAULT_QUOTE_CHARACTER, CSVWriter.DEFAULT_ESCAPE_CHARACTER, CSVWriter.RFC4180_LINE_END)) {
 
-			String[] columns;
-			while ((columns = reader.readNext()) != null) {
-				columns = mask(columns, maskFields);
-				writer.writeNext(columns, false);
-			}
-			writer.flushQuietly();
-		} finally {
-		}
-		System.out.println("データのマスク化が完了しました。");
-	}
+            String[] columns;
+            while ((columns = reader.readNext()) != null) {
+                mask(columns, maskFields);
+                writer.writeNext(columns, false);
+            }
+            writer.flushQuietly();
+        }
+        System.out.println("データのマスク化が完了しました。");
+    }
 
-	private String[] mask(final String[] columns, List<Integer> maskFields) {
-		String result[] = columns;
-		for (Integer field : maskFields) {
-			int idx = field.intValue() - 1;
-			result[idx] = result[idx].chars().mapToObj(c -> {
-				if (StringUtils.isBlank(String.valueOf(c))) {
-					return String.valueOf(c);
-				} else if (StringUtils.equals("　", String.valueOf(c))) {
-					return "　";
-				} else {
-					if (1 == getCharacterWidth(c)) {
-						return "*";
-					} else {
-						return "＊";
-					}
-				}
-			}).collect(Collectors.joining());
-		}
-		return result;
-	}
+    private void mask(String[] columns, List<Integer> maskFields) {
+        for (Integer field : maskFields) {
+            int idx = field - 1;
+            columns[idx] = columns[idx].chars().mapToObj(c -> {
+                if (StringUtils.isBlank(String.valueOf(c))) {
+                    return String.valueOf(c);
+                } else if (StringUtils.equals("　", String.valueOf(c))) {
+                    return "　";
+                } else {
+                    if (1 == getCharacterWidth(c)) {
+                        return "*";
+                    } else {
+                        return "＊";
+                    }
+                }
+            }).collect(Collectors.joining());
+        }
+    }
 
-	private static int getCharacterWidth(int codePoint) {
-		int value = UCharacter.getIntPropertyValue(codePoint, UProperty.EAST_ASIAN_WIDTH);
-		switch (value) {
-		case UCharacter.EastAsianWidth.NARROW:
-		case UCharacter.EastAsianWidth.NEUTRAL:
-		case UCharacter.EastAsianWidth.HALFWIDTH:
-			return 1;
-		case UCharacter.EastAsianWidth.FULLWIDTH:
-		case UCharacter.EastAsianWidth.WIDE:
-			return 2;
-		case UCharacter.EastAsianWidth.AMBIGUOUS:
-			return (StringUtils.containsAny(Locale.getDefault().getLanguage(), "ja", "vi", "kr", "zh")) ? 2 : 1;
-		default:
-			return 1;
-		}
-	}
+    private static int getCharacterWidth(int codePoint) {
+        int value = UCharacter.getIntPropertyValue(codePoint, UProperty.EAST_ASIAN_WIDTH);
+        switch (value) {
+            case UCharacter.EastAsianWidth.FULLWIDTH:
+            case UCharacter.EastAsianWidth.WIDE:
+                return 2;
+            case UCharacter.EastAsianWidth.AMBIGUOUS:
+                return (StringUtils.containsAny(Locale.getDefault().getLanguage(), "ja", "vi", "kr", "zh")) ? 2 : 1;
+            default:
+                return 1;
+        }
+    }
 
 }
